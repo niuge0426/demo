d8df1e0790514b597b95b73a5ab9edffc57c2e1a
niull <niull@NIULL.digitalchina.com>
2018-11-27 09:57:07 +0800

    测试是否搜索到sql文件

--- src/test/java/com/example/demo/TestGitReset.java
+++ src/test/java/com/example/demo/TestGitReset.java
@@ -1,12 +1,31 @@
 package com.example.demo;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+import org.hsqldb.types.Collation;
+
 public class TestGitReset {
 	
 	public static void main(String[] args) {
-		System.out.println("牛璐璐提交的");
+		/*System.out.println("牛璐璐提交的");
 		System.out.println("测试、、、、、、、");
 		System.out.println("test");
-		System.out.println("test");
+		System.out.println("test");*/
+		
+		List<Integer> list = new ArrayList<>();
+		list.add(12312);
+		list.add(12212);
+		list.add(12);
+		list.add(2312);
+		list.add(312);
+		
+		
+//		Collections.sort(list);
+		System.out.println(list.toString());
 	}
 
 }
